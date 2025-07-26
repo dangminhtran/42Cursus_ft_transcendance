073a49a0c1a8e38a9b3734d75dc7887393c35439
Mael Carvalho <maecarva@student.42.fr>
2025-07-26 16:44:52 +0200

    user routes + user managment routes

--- back_end/database/src/routes/user.ts
+++ back_end/database/src/routes/user.ts
@@ -12,7 +12,7 @@ export default async function authRoutes(fastify: FastifyInstance) {
     });
 
     fastify.post('/login', async (request, reply) => {
-        const { email, password } = request.body as { email: string, password: string };
+        const { email } = request.body as { email: string, password: string };
 		const user: User | null = await fastify.getUserByEmail(email);
 
         if (!user) {
---
+++ back_end/database/src/routes/user_managment.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from 'fastify';
+import { User } from '../structs';
+
+export default async function userManagmentRoutes(fastify: FastifyInstance) {
+
+	// send post request to user/getUserByEmail with {"email": "MAIL"} as body
+    fastify.post('/getUserByEmail', async (request, reply) => {
+		const { email } = request.body as { email: string };
+		const user: User | null = await fastify.getUserByEmail(email);
+
+        if (!user)
+            return reply.code(404).send({ error: 'No user identified by this Email.' });
+
+		return user;
+    });
+	// send post request to user/getUserByID with {"userid": "x"} as body
+    fastify.post('/getUserByID', async (request, reply) => {
+        const { userid } = request.body as { userid: number };
+		const user: User | null = await fastify.getUserByID(userid);
+
+        if (!user)
+            return reply.code(404).send({ error: 'No user identified by this ID.' });
+
+		return user;
+    });
+
+	// send post request to user/update/:userid with json user info updated to update in Database
+	fastify.post('/update/:userid', async (request: any, reply: any) => {
+		const userid = request.params.userid;
+		const { profilepicture, email, password, is2FAEnabled, twoFASecret } = request.body as { profilepicture: string, email: string, password: string, is2FAEnabled: number, twoFASecret: string};
+		if (!profilepicture || !email || !password )
+			return reply.code(404).send({ error: 'Missing user identifier.' });
+		let updateduser: User = {
+			id: userid,
+			profilepicture,
+			email,
+			password,
+			is2FAEnabled: is2FAEnabled == 1 ? true : false,
+			twoFASecret,
+			created_at: "",
+			updated_at: ""
+		};
+
+		console.log(updateduser)
+
+		const result: boolean = await fastify.updateUser(updateduser);
+		if (!result)
+			return reply.code(500).send({ error: 'Internal server error.' });
+		
+		return reply.code(204).send({ message: 'User updated.' });
+	})
+}
